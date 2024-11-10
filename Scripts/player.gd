bfa8307d1c5ce6c1967962157e2512d7370ee00c
Draknol <reubenthomas.rr@gmail.com>
2024-11-10 17:51:13 +1300

    Walls with fade on edges added

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,33 +1,34 @@
 extends CharacterBody2D
 
 
+# Instancable variables
 @export var texture: Texture
 @export var speed: float
 @export var camera_zoom: float
-const ASPECT_RATIO = 2
-const DISTORTION_FACTOR = 1 / cos(PI / 6) #magic number
 
 
+# Magic number
+const DISTORTION = 1 / cos(PI / 6)
+
+
+# Set defaults
 func _ready() -> void:
 	$Camera.zoom = Vector2(camera_zoom, camera_zoom)
 	$Sprite.texture = texture
 
-# Update character every frame
+
+# Update every frame
 func _process(delta: float) -> void:
-	var direction = Vector2.ZERO
-	
-	# Get movement direction
-	if Input.is_action_pressed("WalkUp"):
-		direction.y -= 1
-	if Input.is_action_pressed("WalkDown"):
-		direction.y += 1
-	if Input.is_action_pressed("WalkLeft"):
-		direction.x -= ASPECT_RATIO / DISTORTION_FACTOR
-	if Input.is_action_pressed("WalkRight"):
-		direction.x += ASPECT_RATIO / DISTORTION_FACTOR
+	move_and_collide(get_direction() * speed * delta)
+
+
+# Get isometric input direction
+func get_direction() -> Vector2:
+	var direction = Input.get_vector("WalkLeft", "WalkRight", "WalkUp", "WalkDown")
 	
 	# Normalise, accounting for isometric
+	direction.x *= 2 / DISTORTION
 	direction = direction.normalized()
-	direction.x *= DISTORTION_FACTOR
+	direction.x *= DISTORTION
 	
-	move_and_collide(direction * speed * delta)
+	return direction
