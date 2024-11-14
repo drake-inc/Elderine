38ed939b4e8e8b2584c15e7881e8ecd3e95cf653
Draknol <reubenthomas.rr@gmail.com>
2024-11-14 18:39:33 +1300

    Custom menu theme added

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -6,10 +6,17 @@ func _ready() -> void:
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Level.tscn")
 
+func _on_start_mouse_entered() -> void:
+	$VBoxContainer/Start.grab_focus()
 
-func _on_settings_pressed() -> void:
+func _on_option_pressed() -> void:
 	pass # TODO add settings
- 
+
+func _on_options_mouse_entered() -> void:
+	$VBoxContainer/Options.grab_focus()
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+func _on_exit_mouse_entered() -> void:
+	$VBoxContainer/Exit.grab_focus()
